892ee66498c5210645c2baf35d02049d51beaf8b
Emanuel Feliz <emanuel.felizmendez@gmail.com>
2022-06-24 11:17:00 -0400

    configurations files added

---
+++ EMarket/Models/Advertisements.cs
@@ -0,0 +1,43 @@
+﻿using EMarket.Areas.Identity.Pages.Account;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Models
+{
+    public class Advertesiments
+    {
+        [Key]
+        public int AdvertesimentsID { get; set; }
+
+        [ForeignKey("CategorieId")]
+        public virtual Categories Categories { get; set; }
+
+        [ForeignKey("Id")]
+        public virtual LoginModel Login { get; set; }
+
+        [Required]
+        public string ArticleName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Images { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+
+        public DateTime Date { get; set; }
+
+
+
+
+
+    }
+}
---
+++ EMarket/Models/Categories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMarket.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategorieId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Required]  
+        public string Description { get; set; }
+    }
+}
